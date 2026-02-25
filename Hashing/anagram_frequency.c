15566479c4066bb3000f55a493679848ffef19fd
VARSHITH DHARIPALLY <25211a05kq@bvrit.ac.in>
2026-02-25 22:14:17 +0530

    Day 1: Anagram in C
    Comparing both strings using hashmap frequency to check if they are anagram

---
+++ Hashing/anagram_frequency.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[] = "anagram";
+    char t[] = "nagaram";
+    int freq[26] = {0};
+    
+    //Check length of both strings
+   if(strlen(s)!=strlen(t))
+   {
+     printf("Not An Anagram");
+     return 0;
+   }
+  
+  
+    // Traverse both strings
+    for (int i = 0; s[i] != '\0' && t[i] != '\0'; i++)
+    {
+        int index1 = s[i] - 'a';  // map char to index
+        int index2 = t[i] - 'a';
+
+        freq[index1]++;  // increase for s
+        freq[index2]--;  // decrease for t
+    }
+
+
+    // Check frequency array
+    for (int j = 0; j < 26; j++)
+    {
+        if (freq[j] != 0)
+        {
+            printf("Not An Anagram");
+            return 0;
+        }
+    }
+
+    printf("An Anagram");
+    return 0;
+}
